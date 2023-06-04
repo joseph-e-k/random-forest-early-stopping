8dda71b16e52ebdec731c2594c13112a9c3f03d1
joseph-e-k <joseph-e-k@users.noreply.github.com>
2023-06-05 00:47:18 +0300

    Begin exploring the predestined-k approach. Given envelope, find probability of each node in forest progress graph.

--- predestined_k_approach.py
+++ predestined_k_approach.py
@@ -1,83 +1,58 @@
-import dataclasses
-from pprint import pprint
-
 import numpy as np
 
 
-@dataclasses.dataclass(frozen=True)
-class Node:
-    n_positive_so_far: int
-    n_positive_remaining: int
-    n_negative_remaining: int
-    log_probability: float
+def get_negative_observation_probability(n_total, n_total_positive, n_observed, n_observed_positive):
+    n_remaining = n_total - n_observed
+    n_remaining_positive = n_total_positive - n_observed_positive
 
-    @property
-    def n_total_remaining(self):
-        return self.n_positive_remaining + self.n_negative_remaining
-
-    @property
-    def log_p_negative(self):
-        return np.log(self.n_negative_remaining) - np.log(self.n_total_remaining)
-
-    @property
-    def log_p_positive(self):
-        return np.log(self.n_positive_remaining) - np.log(self.n_total_remaining)
-
-    def observe_negative(self):
-        return Node(
-            self.n_positive_so_far,
-            self.n_positive_remaining,
-            self.n_negative_remaining - 1,
-            self.log_probability + self.log_p_negative
-        )
-
-    def observe_positive(self):
-        return Node(
-            self.n_positive_so_far + 1,
-            self.n_positive_remaining - 1,
-            self.n_negative_remaining,
-            self.log_probability + self.log_p_positive
-        )
+    return (n_remaining - n_remaining_positive) / n_remaining
 
 
-def get_n_nodes(n_total, i_layer):
-    if i_layer <= n_total / 2:
-        return i_layer + 1
-    return n_total - i_layer + 1
+def get_positive_observation_probability(n_total, n_total_positive, n_observed, n_observed_positive):
+    n_remaining = n_total - n_observed
+    n_remaining_positive = n_total_positive - n_observed_positive
+
+    return n_remaining_positive / n_remaining
 
 
-def get_probability_network(n_total, n_positive, n_layers=None):
-    if n_layers is None:
-        n_layers = n_total + 1
+def get_probabilities(n_total, n_total_positive, limits=None):
+    n_steps = n_total + 1
+    n_values = n_total_positive + 1
 
-    n_negative = n_total - n_positive
-    layers: list[list[Node]] = [
-        [None] * get_n_nodes(n_total, i_layer)
-        for i_layer in range(n_layers)
-    ]
-    layers[0][0] = Node(0, n_positive, n_negative, np.log(1))
+    if limits is None:
+        limits = [(-np.inf, np.inf)] * n_steps
 
-    for i_layer, layer in enumerate(layers[1:], start=1):
-        last_layer = layers[i_layer-1]
+    probability = [[0] * n_values for _ in range(n_steps)]
+    probability[0][0] = 1
 
-        layer[0] = last_layer[0].observe_negative()
-        layer[-1] = last_layer[-1].observe_positive()
+    for i_step in range(1, n_steps):
+        lower_limit, upper_limit = limits[i_step]
 
-        for i in range(2, len(layer)-1):
-            log_probability = np.log(
-                np.exp(last_layer[i-1].observe_positive().log_probability)
-                + np.exp(last_layer[i].observe_negative().log_probability)
+        if 0 < lower_limit:
+            probability[i_step][0] = 0
+        else:
+            probability[i_step][0] = probability[i_step-1][0] * get_negative_observation_probability(
+                n_total,
+                n_total_positive,
+                i_step - 1,
+                0
             )
 
-            layer[i] = dataclasses.replace(
-                last_layer[i-1].observe_positive(),
-                log_probability=log_probability
-            )
+        for i_value in range(1, n_values):
+            if i_value < lower_limit or upper_limit < i_value:
+                probability[i_step][i_value] = 0
+            else:
+                probability[i_step][i_value] = (
+                    probability[i_step-1][i_value] * get_negative_observation_probability(
+                        n_total, n_total_positive, i_step - 1, i_value
+                    )
+                    + probability[i_step-1][i_value-1] * get_positive_observation_probability(
+                        n_total, n_total_positive, i_step - 1, i_value - 1
+                    )
+                )
 
-    return layers
+    return probability
 
 
 if __name__ == "__main__":
-    pprint(
-        get_probability_network(2, 1)
-    )
+    print(get_probabilities(10, 5))
