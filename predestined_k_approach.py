9109e8868e4935298b8e771fcf39b4edea551ae7
joseph-e-k <joseph-e-k@users.noreply.github.com>
2023-06-05 01:17:34 +0300

    Refactor out a function for computing the probability of an individual node.

--- predestined_k_approach.py
+++ predestined_k_approach.py
@@ -15,44 +15,43 @@ def get_positive_observation_probability(n_total, n_total_positive, n_observed, 
     return n_remaining_positive / n_remaining
 
 
-def get_probabilities(n_total, n_total_positive, limits=None):
+def get_node_probability(n_total, n_total_positive, n_observed, n_observed_positive, prev_layer_probabilities):
+    probability = prev_layer_probabilities[n_observed_positive] * get_negative_observation_probability(
+        n_total, n_total_positive, n_observed - 1, n_observed_positive
+    )
+
+    if n_observed_positive > 0:
+        probability += prev_layer_probabilities[n_observed_positive - 1] * get_positive_observation_probability(
+            n_total, n_total_positive, n_observed - 1, n_observed_positive - 1
+        )
+
+    return probability
+
+
+def get_node_probabilities(n_total, n_total_positive, limits=None):
     n_steps = n_total + 1
     n_values = n_total_positive + 1
 
     if limits is None:
         limits = [(-np.inf, np.inf)] * n_steps
 
-    probability = [[0] * n_values for _ in range(n_steps)]
-    probability[0][0] = 1
+    probabilities = [[0] * n_values for _ in range(n_steps)]
+    probabilities[0][0] = 1
 
     for i_step in range(1, n_steps):
         lower_limit, upper_limit = limits[i_step]
 
-        if 0 < lower_limit:
-            probability[i_step][0] = 0
-        else:
-            probability[i_step][0] = probability[i_step-1][0] * get_negative_observation_probability(
-                n_total,
-                n_total_positive,
-                i_step - 1,
-                0
-            )
+        prev_layer_probabilities = probabilities[i_step - 1]
 
-        for i_value in range(1, n_values):
+        for i_value in range(n_values):
             if i_value < lower_limit or upper_limit < i_value:
-                probability[i_step][i_value] = 0
+                probability = 0
             else:
-                probability[i_step][i_value] = (
-                    probability[i_step-1][i_value] * get_negative_observation_probability(
-                        n_total, n_total_positive, i_step - 1, i_value
-                    )
-                    + probability[i_step-1][i_value-1] * get_positive_observation_probability(
-                        n_total, n_total_positive, i_step - 1, i_value - 1
-                    )
-                )
+                probability = get_node_probability(n_total, n_total_positive, i_step, i_value, prev_layer_probabilities)
+            probabilities[i_step][i_value] = probability
 
-    return probability
+    return probabilities
 
 
 if __name__ == "__main__":
-    print(get_probabilities(10, 5))
+    print(get_node_probabilities(10, 5))
