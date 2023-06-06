a29391b0407dba3eab8a0d9c482f2f8ff9fd71c0
joseph-e-k <joseph-e-k@users.noreply.github.com>
2023-06-06 19:34:51 +0300

    Begin OOP refactor

--- predestined_k_approach.py
+++ predestined_k_approach.py
@@ -1,57 +1,61 @@
+import dataclasses
+
 import numpy as np
 
 
-def get_negative_observation_probability(n_total, n_total_positive, n_observed, n_observed_positive):
-    n_remaining = n_total - n_observed
-    n_remaining_positive = n_total_positive - n_observed_positive
+@dataclasses.dataclass
+class Forest:
+    n_total: int
+    n_total_positive: int
 
-    return (n_remaining - n_remaining_positive) / n_remaining
+    def get_negative_observation_probability(self, n_observed, n_observed_positive):
+        n_remaining = self.n_total - n_observed
+        n_remaining_positive = self.n_total_positive - n_observed_positive
 
+        return (n_remaining - n_remaining_positive) / n_remaining
 
-def get_positive_observation_probability(n_total, n_total_positive, n_observed, n_observed_positive):
-    n_remaining = n_total - n_observed
-    n_remaining_positive = n_total_positive - n_observed_positive
+    def get_positive_observation_probability(self, n_observed, n_observed_positive):
+        n_remaining = self.n_total - n_observed
+        n_remaining_positive = self.n_total_positive - n_observed_positive
 
-    return n_remaining_positive / n_remaining
+        return n_remaining_positive / n_remaining
 
-
-def get_node_probability(n_total, n_total_positive, n_observed, n_observed_positive, prev_layer_probabilities):
-    probability = prev_layer_probabilities[n_observed_positive] * get_negative_observation_probability(
-        n_total, n_total_positive, n_observed - 1, n_observed_positive
-    )
-
-    if n_observed_positive > 0:
-        probability += prev_layer_probabilities[n_observed_positive - 1] * get_positive_observation_probability(
-            n_total, n_total_positive, n_observed - 1, n_observed_positive - 1
+    def get_node_probability(self, n_observed, n_observed_positive, prev_layer_probabilities):
+        probability = prev_layer_probabilities[n_observed_positive] * self.get_negative_observation_probability(
+            n_observed - 1, n_observed_positive
         )
 
-    return probability
+        if n_observed_positive > 0:
+            probability += prev_layer_probabilities[n_observed_positive - 1] * self.get_positive_observation_probability(
+                n_observed - 1, n_observed_positive - 1
+            )
 
+        return probability
 
-def get_node_probabilities(n_total, n_total_positive, limits=None):
-    n_steps = n_total + 1
-    n_values = n_total_positive + 1
+    def get_node_probabilities(self, limits=None):
+        n_steps = self.n_total + 1
+        n_values = self.n_total_positive + 1
 
-    if limits is None:
-        limits = [(-np.inf, np.inf)] * n_steps
+        if limits is None:
+            limits = [(-np.inf, np.inf)] * n_steps
 
-    probabilities = [[0] * n_values for _ in range(n_steps)]
-    probabilities[0][0] = 1
+        probabilities = [[0] * n_values for _ in range(n_steps)]
+        probabilities[0][0] = 1
 
-    for i_step in range(1, n_steps):
-        lower_limit, upper_limit = limits[i_step]
+        for i_step in range(1, n_steps):
+            lower_limit, upper_limit = limits[i_step]
 
-        prev_layer_probabilities = probabilities[i_step - 1]
+            prev_layer_probabilities = probabilities[i_step - 1]
 
-        for i_value in range(n_values):
-            if i_value < lower_limit or upper_limit < i_value:
-                probability = 0
-            else:
-                probability = get_node_probability(n_total, n_total_positive, i_step, i_value, prev_layer_probabilities)
-            probabilities[i_step][i_value] = probability
+            for i_value in range(n_values):
+                if i_value < lower_limit or upper_limit < i_value:
+                    probability = 0
+                else:
+                    probability = self.get_node_probability(i_step, i_value, prev_layer_probabilities)
+                probabilities[i_step][i_value] = probability
 
-    return probabilities
+        return probabilities
 
 
 if __name__ == "__main__":
-    print(get_node_probabilities(10, 5))
+    print(Forest(10, 5).get_node_probabilities())
