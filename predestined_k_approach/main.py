096ee44d933c8f233ff49302f1057361a07cf7c9
joseph-e-k <joseph-e-k@users.noreply.github.com>
2023-06-07 04:31:22 +0300

    Analyse performance of optimal envelope on a range of values for n_total_positive

--- predestined_k_approach/main.py
+++ predestined_k_approach/main.py
@@ -16,7 +16,11 @@ def get_optimal_envelope(n_total, error_rate):
 
 
 if __name__ == "__main__":
-    forest = Forest(100, 51)
-    forest_with_envelope = ForestWithEnvelope(forest, get_optimal_envelope(100, 0.05))
+    n_total = 101
+    envelope = get_optimal_envelope(n_total, 0.05)
 
-    pprint(forest_with_envelope.analyse())
+    for n_positive in range(0, n_total + 1):
+        forest_with_envelope = ForestWithEnvelope.create(n_total, n_positive, envelope)
+        analysis = forest_with_envelope.analyse()
+        print(f"{n_positive} positive / {n_total}: "
+              f"error rate = {analysis.prob_error}, expected runtime = {analysis.expected_runtime}")
