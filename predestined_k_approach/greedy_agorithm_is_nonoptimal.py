bc0b54827570044dbeffad7dda0da582a3a8d089
joseph-e-k <joseph-e-k@users.noreply.github.com>
2023-06-15 21:20:44 +0300

    Update greedy_agorithm_is_nonoptimal.py to something a little less likely to be a floating-point error, and to compare half-envelopes with half-envelopes.

--- predestined_k_approach/greedy_agorithm_is_nonoptimal.py
+++ predestined_k_approach/greedy_agorithm_is_nonoptimal.py
@@ -3,12 +3,13 @@ from predestined_k_approach.main import *
 
 def main():
     bare_forest = Forest(100, 51)
-    custom_envelope = bare_forest.partial_lower_boundary_to_envelope([0] * 8 + [1, 2])
+    custom_envelope = bare_forest.partial_lower_boundary_to_envelope([0] * 5 + [1, 2])
     forest_with_custom_envelope = ForestWithEnvelope(bare_forest, custom_envelope)
     print(f"Custom envelope: {describe_envelope(custom_envelope)}")
     print(custom_envelope_analysis := forest_with_custom_envelope.analyse())
 
-    greedy_envelope = get_greedy_envelope(bare_forest.n_total, custom_envelope_analysis.prob_error)
+    greedy_lower_boundary = bare_forest.get_greedy_lower_boundary(custom_envelope_analysis.prob_error)
+    greedy_envelope = bare_forest.partial_lower_boundary_to_envelope(greedy_lower_boundary)
     forest_with_greedy_envelope = ForestWithEnvelope(bare_forest, greedy_envelope)
     print(f"Greedy envelope: {describe_envelope(greedy_envelope)}")
     print(forest_with_greedy_envelope.analyse())
