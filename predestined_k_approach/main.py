e27edafb74039a2204bff034255884a8121323c6
joseph-e-k <joseph-e-k@users.noreply.github.com>
2023-06-21 02:49:04 +0300

    In main.py, time how long it takes to construct the envelope

--- predestined_k_approach/main.py
+++ predestined_k_approach/main.py
@@ -1,5 +1,6 @@
 from predestined_k_approach.ForestWithEnvelope import ForestWithEnvelope
 from predestined_k_approach.optimization import get_envelope_by_eb_greedily
+from predestined_k_approach.utils import TimerContext
 
 
 def describe_envelope(envelope):
@@ -18,7 +19,8 @@ def main():
     n_total = 101
     allowable_error = 0.05
 
-    envelope = get_envelope_by_eb_greedily(n_total, allowable_error)
+    with TimerContext("envelope"):
+        envelope = get_envelope_by_eb_greedily(n_total, allowable_error)
 
     print(f"Envelope: stop if {describe_envelope(envelope)}")
 
@@ -26,6 +28,7 @@ def main():
         forest_with_envelope = ForestWithEnvelope.create(n_total, n_positive, envelope)
         analysis = forest_with_envelope.analyse()
         score = forest_with_envelope.get_score(allowable_error)
+
         print(f"{n_positive} positive / {n_total}: "
               f"error rate = {analysis.prob_error}, "
               f"expected runtime = {analysis.expected_runtime}, "
