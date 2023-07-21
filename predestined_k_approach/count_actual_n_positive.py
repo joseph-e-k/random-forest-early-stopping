83295335cbb472f10db405c0c4b6bb1ecef4bb85
joseph-e-k <joseph-e-k@users.noreply.github.com>
2023-07-21 23:53:58 +0300

    Add script to estimate and plot the actual distribution of the number of the number of positive trees in a classification forest

---
+++ predestined_k_approach/count_actual_n_positive.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.utils import Bunch
+
+
+def to_binary_classifications(classifications):
+    classes = set(classifications)
+    n_classes = len(classes)
+
+    if n_classes < 2:
+        raise ValueError("Cannot make binary classification from fewer than 2 classes")
+
+    positive_classes = np.random.choice(np.array(list(classes)), size=n_classes // 2, replace=False)
+
+    return np.isin(classifications, positive_classes)
+
+
+def estimate_positive_tree_distribution(dataset, target_column=-1, n_trees=100, test_proportion=0.2):
+    if isinstance(dataset, tuple) and len(dataset) == 2:
+        X, y = dataset
+    if isinstance(dataset, Bunch):
+        X, y = dataset.data, dataset.target
+    elif isinstance(dataset, np.ndarray):
+        X_columns = tuple(j for j in range(dataset.shape[1]) if j != target_column)
+        X = dataset[:, X_columns]
+        y = dataset[:, target_column]
+    else:
+        raise TypeError(f"expected dataset to be a Bunch, ndarray, or pair; got {dataset!r} instead")
+
+    y = to_binary_classifications(y)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_proportion)
+
+    # Train a random forest classifier
+    rf_classifier = RandomForestClassifier(n_estimators=n_trees)
+    rf_classifier.fit(X_train, y_train)
+
+    # Count number of "positive" trees for each testing observation, and add them up
+    tree_predictions = np.column_stack([tree.predict(X_test) for tree in rf_classifier.estimators_])
+    n_positive_trees = np.sum(tree_predictions, axis=1)
+
+    return n_positive_trees
+
+
+if __name__ == "__main__":
+    datasets = {
+        "Banknotes": np.loadtxt(fname=r"..\data\data_banknote_authentication.txt", delimiter=","),
+        "Breast Cancer": datasets.load_breast_cancer(),
+        "Iris": datasets.load_iris(),
+        "Wine": datasets.load_wine(),
+        "Digits": datasets.load_digits()
+    }
+
+    n_forests_per_dataset = 3
+
+    fig, axs = plt.subplots(len(datasets), n_forests_per_dataset, tight_layout=True)
+
+    for i_dataset, (dataset_name, dataset) in enumerate(datasets.items()):
+        for i_forest in range(n_forests_per_dataset):
+            distribution = estimate_positive_tree_distribution(dataset)
+            axs[i_dataset, i_forest].hist(distribution)
+            axs[i_dataset, i_forest].title.set_text(f"{dataset_name} ({i_forest + 1})")
+
+    plt.show()
