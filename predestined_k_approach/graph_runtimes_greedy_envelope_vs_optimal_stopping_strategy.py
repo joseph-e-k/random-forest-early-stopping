bde5b0f0e31512b38059ca7a80ceb0d0947b61c2
joseph-e-k <joseph-e-k@users.noreply.github.com>
2023-12-05 12:24:58 +0200

    Add some files for debugging the SKY adaptation

---
+++ predestined_k_approach/graph_runtimes_greedy_envelope_vs_optimal_stopping_strategy.py
@@ -0,0 +1,70 @@
+import dataclasses
+import subprocess
+import os
+
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import optimize, stats
+from diskcache import Cache
+
+from .Forest import Forest
+from .optimization import get_optimal_stopping_strategy
+from .ForestWithEnvelope import ForestWithEnvelope
+from .ForestWithStoppingStrategy import ForestWithGivenStoppingStrategy
+from .utils import TimerContext, plot_function, plot_functions, timed
+
+
+cache = Cache(os.path.join(os.path.dirname(__file__), ".cache"))
+
+def time_computation_of_optimal_stopping_strategy(n_total, aer):
+    with TimerContext(f"get_optimal_stopping_strategy({n_total, aer})") as timer:
+        get_optimal_stopping_strategy(n_total, aer)
+    return timer.elapsed_time
+
+@timed
+@cache.memoize()
+def optimal_expected_runtime(n_total, prop_positive, aer, relative=False):
+    stopping_strategy = get_optimal_stopping_strategy(n_total, aer)
+    fwss = ForestWithGivenStoppingStrategy(Forest(n_total, int(n_total * prop_positive)), stopping_strategy)
+    expected_runtime = fwss.analyse().expected_runtime
+
+    if relative:
+        return expected_runtime / n_total
+    return expected_runtime
+
+
+@timed
+@cache.memoize()
+def greedy_envelope_expected_runtime(n_total, prop_positive, aer, relative=False):
+    fwe = ForestWithEnvelope.create_greedy(n_total, int(n_total * prop_positive), aer)
+    expected_runtime = fwe.analyse().expected_runtime
+
+    if relative:
+        return expected_runtime / n_total
+    return expected_runtime
+
+
+def main():
+    n_total_min = 11
+    n_total_max = 251
+    n_total_step = 2
+    prop_positive = 0.25
+    aer = 10**-3
+
+    plot_functions(
+        ax=plt.subplot(),
+        x_axis_arg_name="n_total",
+        functions=[optimal_expected_runtime, greedy_envelope_expected_runtime],
+        function_kwargs=dict(
+            n_total=range(n_total_min, n_total_max, n_total_step),
+            prop_positive=prop_positive,
+            aer=aer,
+            relative=False,
+        )
+    )
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ predestined_k_approach/sky_debug_helpers.py
@@ -0,0 +1,145 @@
+import dataclasses
+
+import numpy as np
+from scipy import optimize, stats
+
+from predestined_k_approach.Forest import Forest
+from predestined_k_approach.optimization import get_optimal_stopping_strategy
+from predestined_k_approach.ForestWithEnvelope import ForestWithEnvelope
+from predestined_k_approach.ForestWithStoppingStrategy import ForestWithGivenStoppingStrategy
+from predestined_k_approach.utils import TimerContext
+
+
+def main1():
+    res = optimize.linprog(
+        c=-np.array([1, 2]),
+        A_ub=np.array([[1, 1]]),
+        b_ub=1
+    )
+    print(res.x)
+    print(-res.fun)
+
+
+def main2():
+    n_total = np.array([20, 30])
+    n_good = np.array([10, 5])
+    n_selected = np.array([3, 4])
+
+    rvs = stats.hypergeom(n_total, n_good, n_selected)
+    print(rvs)
+
+
+@dataclasses.dataclass
+class DummyProblem:
+    conditions_with_labels: list[tuple[bool, str]] = dataclasses.field(default_factory=list)
+
+    def __iadd__(self, condition_with_label: tuple[bool, str]):
+        self.conditions_with_labels.append(condition_with_label)
+        return self
+
+    def check_conditions(self):
+        for condition, label in self.conditions_with_labels:
+            assert condition, label
+
+
+def get_pi_and_pi_bar_from_theta(theta: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+    theta = np.copy(theta)
+    theta.resize((theta.shape[0], theta.shape[0]))
+
+    theta_bar = 1 - theta
+    pi = np.zeros((theta.shape[0] - 1, theta.shape[0]))
+    pi_bar = np.ones_like(pi)
+
+    for c in range(pi.shape[1]):
+        pi[0, c] = theta[0, c]
+        pi_bar[0, c] = theta_bar[0, c]
+
+    for n in range(1, pi.shape[0]):
+        for c in range(pi.shape[1]):
+            combinatoric_factor = (c / n * pi_bar[n-1, c-1] + (n - c) / n * pi_bar[n-1, c])
+            pi[n, c] = theta[n, c] * combinatoric_factor
+            pi_bar[n, c] = theta_bar[n, c] * combinatoric_factor
+
+    return pi, pi_bar
+
+
+def get_expected_runtime_coefficients(t, t_plus):
+    m_pi = np.zeros((t, t_plus + 1))
+
+    for n in range(m_pi.shape[0]):
+        for c in range(m_pi.shape[1]):
+            m_pi[n, c] = n * stats.hypergeom(t, t_plus, n).pmf(c)
+
+    return m_pi
+
+
+def get_expected_runtime(t, t_plus, pi, pi_bar):
+    assert pi.shape == pi_bar.shape
+
+    m_pi = get_expected_runtime_coefficients(t, t_plus)
+
+    r = np.sum(pi * m_pi)
+
+    for c in range(pi.shape[1]):
+        combinatoric_factor = (c / t * pi_bar[t - 1, c - 1] + (t - c) / t * pi_bar[t - 1, c])
+        r += combinatoric_factor * stats.hypergeom(t, t_plus, t).pmf(c) * t
+
+    return r
+
+def get_error_rate(t, t_plus, pi, pi_bar):
+    assert pi.shape == pi_bar.shape
+
+    is_error = np.empty_like(pi, dtype=bool)
+    for n in range(is_error.shape[0]):
+        for c in range(is_error.shape[1]):
+            is_error[n, c] = ((c > n / 2) != (t_plus > t / 2))
+
+    s = 0
+
+    for n in range(pi.shape[0]):
+        for c in range(pi.shape[1]):
+            if is_error[n, c]:
+                s += stats.hypergeom(t, t_plus, n).pmf(c) * pi[n, c]
+
+    for c in range(pi_bar.shape[1]):
+        if is_error[t, c]:
+            s += stats.hypergeom(t, t_plus, t).pmf(c) * pi_bar[t, c]
+
+    return s
+
+
+def get_greedy_fwe_pi_and_pi_bar():
+    n_total = 185
+    fwe = ForestWithEnvelope.create_greedy(n_total, n_total // 2, 0.001)
+    return get_pi_and_pi_bar_from_theta(np.exp(fwe._get_log_prob_stop()))
+
+
+def main():
+    aer = 0.001
+
+    for n_total in [185]:
+        n_positive = n_total // 2
+
+        forest = Forest(n_total, n_positive)
+
+        with TimerContext(f"find optimal stopping strategy ({n_total=}, {aer=})"):
+            optimal_stopping_strategy = get_optimal_stopping_strategy(n_total, aer)
+
+        fwss = ForestWithGivenStoppingStrategy(forest, optimal_stopping_strategy)
+        fwe = ForestWithEnvelope.create_greedy(n_total, n_positive, aer)
+
+        fwss_analysis = fwss.analyse()
+        fwe_analysis = fwe.analyse()
+
+        print(f"{fwss_analysis=})")
+        print(f"{fwe_analysis=})")
+
+        fwss_pi, fwss_pi_bar = get_pi_and_pi_bar_from_theta(fwss.stopping_strategy)
+        fwe_pi, fwe_pi_bar = get_pi_and_pi_bar_from_theta(np.exp(fwe._get_log_prob_stop()))
+
+        print(f"{get_expected_runtime(n_total, n_total // 2, fwss_pi, fwss_pi_bar)=}")
+        print(f"{get_expected_runtime(n_total, n_total // 2, fwe_pi, fwe_pi_bar)=}")
+
+
+if __name__ == "__main__":
+    main()
