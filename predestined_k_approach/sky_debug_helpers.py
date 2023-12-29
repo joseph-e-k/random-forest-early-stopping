22b18f9ab437a98311b3c461c72023d82ba224d6
joseph-e-k <joseph-e-k@users.noreply.github.com>
2023-12-29 14:46:10 +0200

    In sky_debug_helpers.py, increase size of process pool to 256 and print more information when a noteworthy result is encountered

--- predestined_k_approach/sky_debug_helpers.py
+++ predestined_k_approach/sky_debug_helpers.py
@@ -56,12 +56,12 @@ def get_expected_runtimes(n_total, aer=10**-6):
 def main():
     n_totals = range(11, 1001, 2)
     with TimerContext():
-        with mp.Pool(64) as pool:
-            for (args, times) in pool.imap(get_expected_runtimes, n_totals, ):
+        with mp.Pool(256) as pool:
+            for (args, times) in pool.imap(get_expected_runtimes, n_totals):
                 n_total, _ = args
                 low_fwss_time, high_fwss_time, low_fwe_time, high_fwe_time = times
                 if low_fwss_time > low_fwe_time and high_fwss_time > high_fwe_time:
-                    print(n_total)
+                    print(f"* {n_total=}, {low_fwss_time=}, {high_fwss_time=}, {low_fwe_time=}, {high_fwe_time=}")
 
 
 if __name__ == "__main__":
