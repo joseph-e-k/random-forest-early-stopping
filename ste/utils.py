3b4b20e8d9c30c7d745545fb21d370074ee7974f
joseph-e-k <joezkal@gmail.com>
2024-06-22 09:31:47 +0300

    Allow users of `memoize` to specify arguments that should be ignored w.r.t. caching

--- ste/utils.py
+++ ste/utils.py
@@ -114,19 +114,21 @@ def is_proportion_surprising(observations, expected_proportion, confidence_level
     return stats.binomtest(sum(observations), len(observations), expected_proportion).pvalue < 1 - confidence_level
 
 
-def _robust_cache_key(function, name, *args, **kwargs):
+def _robust_cache_key(function, name, args_to_ignore, *args, **kwargs):
     signature = inspect.signature(function)
     bound_arguments = signature.bind(*args, **kwargs)
     bound_arguments.apply_defaults()
-    return (name,) + tuple(bound_arguments.arguments.items())
+    return (name,) + tuple(
+        (key, value) for (key, value) in bound_arguments.arguments.items() if key not in args_to_ignore
+    )
 
 
-def memoize(name=None):
-    def decorator(function, name=name):
+def memoize(name=None, args_to_ignore=()):
+    def decorator(function, name=name, args_to_ignore=args_to_ignore):
         if name is None:
             name = full_name(function)
         memoized = cache.memoize(name=name)(function)
-        memoized.__cache_key__ = functools.partial(_robust_cache_key, memoized, name)
+        memoized.__cache_key__ = functools.partial(_robust_cache_key, memoized, name, args_to_ignore)
         return memoized
     return decorator
 
