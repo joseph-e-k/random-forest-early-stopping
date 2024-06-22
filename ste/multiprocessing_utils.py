075d53a7a8bd7c2e713c0fe7e75f96a6bfc1c3cf
joseph-e-k <joezkal@gmail.com>
2024-06-22 09:33:02 +0300

    Add arguments to `parallelize`: - `fixed_args`, a fixed tuple of arguments passed to every task (supplementing the iterable of argument tuples that change between every task) - `n_workers`, which (if provided) overrides the constant `N_WORKER_PROCESSES` to specify the number of worker processes to be used - `verbose`, a flag indicating that completed tasks to log their completion to console along with a count of tasks completed

--- ste/multiprocessing_utils.py
+++ ste/multiprocessing_utils.py
@@ -1,8 +1,10 @@
 import dataclasses
+import datetime
 import multiprocessing as mp
 import os
 import traceback
 from collections.abc import Mapping
+from ctypes import c_uint
 from typing import Callable
 
 from ste.utils import TimerContext
@@ -11,9 +13,22 @@ _ENV_KEY_N_WORKER_PROCESSES = "STE_N_WORKER_PROCESSES"
 N_WORKER_PROCESSES = int(os.getenv(_ENV_KEY_N_WORKER_PROCESSES, 32))
 
 
+class Counter:
+    def __init__(self, manager):
+        self.value = manager.Value(c_uint, 0)
+        self.lock = manager.RLock()
+
+    def increment(self):
+        with self.lock:
+            new_value = self.value.get() + 1
+            self.value.set(new_value)
+            return new_value
+
+
 @dataclasses.dataclass(frozen=True)
 class _CallableForWorkerProcesses:
     function: Callable
+    counter: Counter = None
 
     def __call__(self, args_or_kwargs):
         try:
@@ -25,9 +40,24 @@ class _CallableForWorkerProcesses:
         except Exception as e:
             traceback.print_exc()
             return args_or_kwargs, False, e, timer.elapsed_time
-        return args_or_kwargs, True, result, timer.elapsed_time
+        else:
+            return args_or_kwargs, True, result, timer.elapsed_time
+        finally:
+            self.finish()
+    
+    def finish(self):
+        if self.counter is not None:
+            n_completed = self.counter.increment()
+            print(f"{datetime.datetime.utcnow().isoformat()}:  Completed {n_completed} tasks")
 
 
-def parallelize(function, argses):
-    with mp.Pool(N_WORKER_PROCESSES) as pool:
-        yield from pool.imap(_CallableForWorkerProcesses(function), argses)
+
+def parallelize(function, iter_argses, fixed_args=(), n_workers=N_WORKER_PROCESSES, verbose=False):
+    manager = mp.Manager()
+    counter = Counter(manager) if verbose else None
+
+    with mp.Pool(n_workers) as pool:
+        yield from pool.imap(
+            _CallableForWorkerProcesses(function, counter),
+            (iter_args + fixed_args for iter_args in iter_argses)
+        )
